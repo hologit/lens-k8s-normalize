5c1bbfffa447947adbe29fe83b4c8e794a0d523e
Chris Alfano <chris@jarv.us>
2020-07-25 19:50:21 +0000

    feat: add initial implementation

---
+++ plan.sh
@@ -0,0 +1,35 @@
+pkg_name=lens-k8s-normalize
+pkg_origin=holo
+pkg_version="0.1.0"
+pkg_maintainer="Chris Alfano <chris@jarv.us>"
+pkg_license=("MIT")
+pkg_build_deps=(
+)
+pkg_deps=(
+  core/node
+)
+pkg_bin_dirs=(bin)
+
+
+do_build() {
+  pushd "${CACHE_PATH}" > /dev/null
+
+  build_line "Generating lens script"
+  mkdir -v "bin"
+  cp -v "${PLAN_CONTEXT}/bin"/* "./bin/"
+  fix_interpreter "bin/*" core/node bin/node
+
+  build_line "Running: npm install"
+  cp -v "${PLAN_CONTEXT}"/package{,-lock}.json ./
+  npm ci
+
+  popd > /dev/null
+}
+
+do_install() {
+  cp -r "${CACHE_PATH}"/* "${pkg_prefix}/"
+}
+
+do_strip() {
+  return 0
+}
